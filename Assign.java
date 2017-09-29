bec4c2088580ecd868d986fd9253692115da7a28
sakinahdewi <32097423+sakinahdewi@users.noreply.github.com>
2017-09-29 00:30:57 -0600

    Add files via upload

---
+++ Assign.java
@@ -0,0 +1,29 @@
+public class Assign {
+	
+	public static void main(String args[] ) {
+		boolean b=true;
+		System.out.println("Value b = " + b);
+		
+		char c='C';
+		System.out.println("Value c = " + c);
+		
+		byte bt=10;
+		System.out.println("Value bt = " + bt);
+		
+		short a=20;
+		System.out.println("Value a = " + a);
+		
+		int i=30;
+		System.out.println("Value i = " + i);
+		
+		long l=40L;
+		System.out.println("Value l = " + l);
+		
+		float f=3.14F;
+		System.out.println("Value f = " + f);
+		
+		double d=3.14;
+		System.out.println("Value d = " + d);
+		
+	}
+}
---
+++ Invest.java
@@ -0,0 +1,14 @@
+public class Invest {
+	
+	public static void main(String[] arguments) {
+		float total = 14000;
+		System.out.println("Original Investment: $" + total);
+		
+		System.out.println("After one year: $" + total);
+		
+		System.out.println("After two year: $" + total);
+		
+		System.out.println("After three year: $" + total);
+		
+	}
+}
